e7e61eff8a85876df7582751605bf162d68d03f1
MarkPerez <markperez88@hotmail.co.uk>
2016-06-13 15:10:31 +0100

    Example 3

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+//Event Listener
+var event1 = document.getElementById("button1");
+event1.addEventListener('click',myfirst,false);
+
+function myfirst() {
+//get user input 1
+var userinput1 = document.getElementById("input1").value;
+//write output 1 onto webpage
+document.getElementById("demo").innerHTML = userinput1;
+    }
