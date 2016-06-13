7365efa1edc0c7d6a1f0a56ddc181387b3bbaee4
MarkPerez <markperez88@hotmail.co.uk>
2016-06-13 15:41:11 +0100

    Example 4

--- Javascript.js
+++ Javascript.js
@@ -3,8 +3,8 @@ var event1 = document.getElementById("button1");
 event1.addEventListener('click',myfirst,false);
 
 function myfirst() {
-//get user input 1
-var userinput1 = document.getElementById("input1").value;
-//write output 1 onto webpage
-document.getElementById("demo").innerHTML = userinput1;
-    }
+var userinput1 = parseInt(document.getElementById("input1").value);
+var userinput2 = parseInt(document.getElementById("input2").value); 
+
+document.getElementById("demo").innerHTML = userinput1 + userinput2;
+}
