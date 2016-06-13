0fd2c2ada339f0e86d7023e0a216044bbdf347f7
MarkPerez <markperez88@hotmail.co.uk>
2016-06-13 16:03:31 +0100

    Example 5

--- Javascript.js
+++ Javascript.js
@@ -6,5 +6,15 @@ function myfirst() {
 var userinput1 = parseInt(document.getElementById("input1").value);
 var userinput2 = parseInt(document.getElementById("input2").value); 
 
-document.getElementById("demo").innerHTML = userinput1 + userinput2;
+if (userinput1 > userinput2){
+ document.getElementById("demo").innerHTML = userinput1;   
 }
+    else if(userinput2 > userinput1){
+   document.getElementById("demo").innerHTML = userinput2;     
+    }
+    else{
+     document.getElementById("demo").innerHTML = "the numbers are equal";   
+    }
+
+}
+
